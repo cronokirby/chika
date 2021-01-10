309c27e7002abca6f4291ef3bc2be25a16f2aae2
Lúcás Meier <cronokirby@gmail.com>
2021-01-11 00:18:01 +0100

    Add command line parsing
    We use StructOpt to structure our parsing around our argument data
    structure.
    
    For now, we just print out this data structure, without doing anything
    interesting.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+/// A command that our CLI can process.
+#[derive(Debug, StructOpt)]
+enum Command {
+    /// Print the tokens produced by the lexer
+    Lex {
+        /// The file containing Chika code you want to lex
+        #[structopt(name = "INPUT_FILE", parse(from_os_str))]
+        input_file: PathBuf,
+    },
+    /// Print the AST produced by the parser
+    Parse {
+        /// The file containing Chika code you want to parse
+        #[structopt(name = "INPUT_FILE", parse(from_os_str))]
+        input_file: PathBuf,
+    },
+    /// Print the simplified AST
+    Simplify {
+        /// The file containing Chika code you want to simplify
+        #[structopt(name = "INPUT_FILE", parse(from_os_str))]
+        input_file: PathBuf,
+    },
+    /// Run the type checker, printing the typed AST
+    TypeCheck {
+        /// The file containing Chika code you want to check
+        #[structopt(name = "INPUT_FILE", parse(from_os_str))]
+        input_file: PathBuf,
+    },
+    /// Compile the file
+    Compile {
+        /// The file containing Chika code you want to check
+        #[structopt(name = "INPUT_FILE", parse(from_os_str))]
+        input_file: PathBuf,
+        /// The output file to use
+        #[structopt(short, long, parse(from_os_str))]
+        output: PathBuf,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Command::from_args();
+    println!("{:#?}", args);
 }
