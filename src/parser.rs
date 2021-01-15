f93e25867b4bebebc7ad8a304fed2c717ab7793a
Lúcás Meier <cronokirby@gmail.com>
2021-01-15 21:10:03 +0100

    Define node shape separately
    This allows us to store strings and nodes

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,18 @@
-use crate::context::Location;
+use crate::context::{Location, StringID};
+
+/// Represents the kind of shape that a node can have.
+///
+/// We need this, because everything we have is stored in this unified
+/// node type, and we need to handle terminal things like strings and literals.
+#[derive(Debug)]
+enum NodeShape {
+    /// This node is a terminal reference to a string
+    String(StringID),
+    /// This node is a terminal reference to an integer
+    IntLit(u32),
+    /// This node branches off to contain other nodes
+    Branch(Vec<Node>),
+}
 
 /// Represents a single raw Node in our AST.
 ///
@@ -9,6 +23,29 @@ use crate::context::Location;
 struct Node {
     /// The location of this node in the original file
     location: Location,
-    /// The nodes that this node references
-    children: Vec<Node>,
+    /// The shape of this node
+    shape: NodeShape,
+}
+
+impl Node {
+    fn string(&self) -> StringID {
+        match &self.shape {
+            NodeShape::String(s) => *s,
+            other => panic!("expected string, found: {:?}", other),
+        }
+    }
+
+    fn int_lit(&self) -> u32 {
+        match &self.shape {
+            NodeShape::IntLit(v) => *v,
+            other => panic!("expected int lit, found: {:?}", other),
+        }
+    }
+
+    fn branch(&self) -> &[Node] {
+        match &self.shape {
+            NodeShape::Branch(v) => v,
+            other => panic!("expected branch, found: {:?}", other),
+        }
+    }
 }
