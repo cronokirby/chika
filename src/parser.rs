27a23df0a40e6304ceb8b7282e23f9200b4357ad
Lúcás Meier <cronokirby@gmail.com>
2021-01-15 22:04:21 +0100

    Define AST for expressions

--- src/parser.rs
+++ src/parser.rs
@@ -5,6 +5,18 @@ use crate::{
     types::BuiltinType,
 };
 
+#[derive(Debug)]
+#[repr(u8)]
+enum Tag {
+    IntLitExpr,
+    BinExprAdd,
+    BinExprMul,
+    BinExprSub,
+    BinExprDiv,
+    VarExpr,
+    Expr,
+}
+
 /// Represents the kind of shape that a node can have.
 ///
 /// We need this, because everything we have is stored in this unified
@@ -30,6 +42,8 @@ enum NodeShape {
 struct Node {
     /// The location of this node in the original file
     location: Location,
+    /// A tag associated with this node
+    tag: Tag,
     /// The shape of this node
     shape: NodeShape,
 }
@@ -61,5 +75,117 @@ impl Node {
             NodeShape::Branch(v) => v,
             other => panic!("expected branch, found: {:?}", other),
         }
+    }
+}
+
+#[derive(Debug)]
+pub enum ExprKind {
+    IntLitExpr(IntLitExpr),
+    VarExpr(VarExpr),
+    BinExpr(BinExpr),
+}
+
+#[derive(Debug)]
+pub struct Expr(Rc<Node>);
+
+impl Expr {
+    fn kind(&self) -> ExprKind {
+        match &self.0.tag {
+            Tag::VarExpr => VarExpr(self.0.clone()).into(),
+            Tag::IntLitExpr => IntLitExpr(self.0.clone()).into(),
+            Tag::BinExprAdd => BinExpr::add(self.0.clone()).into(),
+            Tag::BinExprSub => BinExpr::sub(self.0.clone()).into(),
+            Tag::BinExprMul => BinExpr::mul(self.0.clone()).into(),
+            Tag::BinExprDiv => BinExpr::div(self.0.clone()).into(),
+            other => panic!("unexpected tag {:?}", other),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct IntLitExpr(Rc<Node>);
+
+impl IntLitExpr {
+    pub fn int_lit(&self) -> u32 {
+        self.0.int_lit()
+    }
+}
+
+impl Into<ExprKind> for IntLitExpr {
+    fn into(self) -> ExprKind {
+        ExprKind::IntLitExpr(self)
+    }
+}
+
+#[derive(Debug)]
+pub struct VarExpr(Rc<Node>);
+
+impl VarExpr {
+    pub fn var(&self) -> StringID {
+        self.0.string()
+    }
+}
+
+impl Into<ExprKind> for VarExpr {
+    fn into(self) -> ExprKind {
+        ExprKind::VarExpr(self)
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum BinOp {
+    Add,
+    Mul,
+    Sub,
+    Div,
+}
+
+#[derive(Debug)]
+pub struct BinExpr {
+    pub op: BinOp,
+    node: Rc<Node>,
+}
+
+impl BinExpr {
+    fn add(node: Rc<Node>) -> Self {
+        BinExpr {
+            op: BinOp::Add,
+            node,
+        }
+    }
+
+    fn sub(node: Rc<Node>) -> Self {
+        BinExpr {
+            op: BinOp::Sub,
+            node,
+        }
+    }
+
+    fn mul(node: Rc<Node>) -> Self {
+        BinExpr {
+            op: BinOp::Mul,
+            node,
+        }
+    }
+
+    fn div(node: Rc<Node>) -> Self {
+        BinExpr {
+            op: BinOp::Div,
+            node,
+        }
+    }
+
+    fn lhs(&self) -> Expr {
+        Expr(self.node.branch()[0].clone())
+    }
+
+    fn rhs(&self) -> Expr {
+        Expr(self.node.branch()[1].clone())
+    }
+}
+
+impl Into<ExprKind> for BinExpr {
+    fn into(self) -> ExprKind {
+        ExprKind::BinExpr(self)
     }
 }
