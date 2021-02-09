e103ff62110a50792079b4dbef3ebaa43b7e7a0f
Lúcás Meier <cronokirby@gmail.com>
2021-02-09 20:17:16 +0100

    Define a Writer struct

--- src/backend/c.rs
+++ src/backend/c.rs
@@ -1,12 +1,40 @@
 use crate::analysis::AST;
 use crate::errors::Error;
+use io::Write;
 use std::io;
 
-pub fn generate(writer: &mut dyn io::Write, _ast: AST) -> Result<(), Error> {
-    writeln!(writer, "#include <stdio.h>\n")?;
-    writeln!(writer, "int main() {{")?;
-    writeln!(writer, "  puts(\"Hello from Chika!\");")?;
-    writeln!(writer, "  return 0;")?;
-    writeln!(writer, "}}")?;
-    Ok(())
+/// A struct capable of writing things out to a C file.
+///
+/// This houses utilities and context making things code generation a bit easier.
+struct Writer<'a> {
+    writer: &'a mut dyn io::Write,
+}
+
+impl<'a> Writer<'a> {
+    fn new(writer: &'a mut dyn io::Write) -> Self {
+        Writer { writer }
+    }
+
+    fn generate(&mut self, _ast: AST) -> Result<(), Error> {
+        writeln!(self, "#include <stdio.h>\n")?;
+        writeln!(self, "int main() {{")?;
+        writeln!(self, "  puts(\"Hello from Chika!\");")?;
+        writeln!(self, "  return 0;")?;
+        writeln!(self, "}}")?;
+        Ok(())
+    }
+}
+
+impl<'a> io::Write for Writer<'a> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.writer.write(buf)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.writer.flush()
+    }
+}
+
+pub fn generate(writer: &mut dyn io::Write, ast: AST) -> Result<(), Error> {
+    Writer::new(writer).generate(ast)
 }
