16c05bae10df9a66cbf7162f7dcb26247818f76f
Lúcás Meier <cronokirby@gmail.com>
2021-02-13 17:48:08 +0100

    Setup integration testing suite

---
+++ integration_tests.py
@@ -0,0 +1,88 @@
+"""
+This file runs all of the integration tests in the directory `tests`
+"""
+import os
+import re
+import subprocess
+
+ROOT = "tests"
+OUTPUT = ".output.c"
+
+
+def get_expected(file_name):
+    """
+    Return the expected output for a given file.
+    """
+    expected = []
+    with open(file_name) as fp:
+        for line in fp:
+            match = re.search(r"//.*OUT\((.*)\)", line)
+            if match:
+                expected.append(match.group(1))
+    return "\n".join(expected)
+
+
+def gen_executable(file_name):
+    """
+    Generate an executable for a given file
+    """
+    out = subprocess.check_output(
+        f"cargo run -- compile {file_name} -o .out/out.c", shell=True
+    ).decode("utf-8")
+    if "Error" in out:
+        raise Exception("\n".join(out.split("\n")[1:]))
+    subprocess.run(
+        "gcc -std=c99 -fsanitize=address -fsanitize=undefined .out/out.c -o .out/a.out",
+        shell=True,
+        check=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+
+
+def get_output(file_name):
+    """
+    Get the output for a given file
+    """
+    gen_executable(file_name)
+    return subprocess.check_output(".out/a.out", shell=True).decode("utf-8")
+
+
+def file_names():
+    """
+    Yield all of the file names we need to run an integration test on
+    """
+    for file_name in os.listdir(ROOT):
+        if file_name.endswith(".chika"):
+            yield os.path.join(ROOT, file_name)
+
+
+def main():
+    """
+    The main function for our script.
+    This will find all of the integration tests to run, and then
+    run them in order, checking that their expected output matches
+    what gets printed.
+    """
+    for name in sorted(list(file_names())):
+        expected = get_expected(name).strip()
+        try:
+            actual = get_output(name).strip()
+        except BaseException as err:
+            print(f"{name}: FAIL")
+            print("Exception:")
+            print(err)
+            continue
+        if expected == actual:
+            print(f"\033[1m{name}\033[0m:\t\033[1m\033[32mPASS\033[0m")
+        else:
+            print(f"\033[1m{name}\033[0m:\t\033[1m\033[31mFAIL\033[0m")
+            print("Expected:\033[1m")
+            print(expected)
+            print("\033[0m\nBut Found:\033[1m")
+            print(actual)
+            print("\033[0m")
+
+
+if __name__ == "__main__":
+    main()
